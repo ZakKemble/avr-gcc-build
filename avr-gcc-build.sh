a09ed193e1c37cad87d09136d0c0a65a3837a6a8
Zak Kemble <zakkemble@gmail.com>
2022-05-11 16:04:38 +0100

    Some formatting

--- avr-gcc-build.sh
+++ avr-gcc-build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Project: avr-gcc-build
+# Author: Zak Kemble, contact@zakkemble.net
+# Copyright: (C) 2022 by Zak Kemble
+# Web: https://blog.zakkemble.net/avr-gcc-builds/
+
+# License:
+# Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+# http://creativecommons.org/licenses/by-sa/4.0/
+
+
 # http://www.nongnu.org/avr-libc/user-manual/install_tools.html
 
-# For optimum compile time this should generally be set to the number of CPU cores your machine has
+# For optimum compile time this should generally be set to the number of CPU cores your machine has.
+# Some systems with not much RAM may fail with "collect2: fatal error: ld terminated with signal 9 [Killed]", if this happens try reducing the JOBCOUNT value.
+# In my case Debian 10 builds GCC 10.1.0 fine with 4 cores and 2GB RAM, but Debian 11 needs 5.5GB?
 JOBCOUNT=$(getconf _NPROCESSORS_ONLN)
 
 # Build for Linux
@@ -73,11 +85,15 @@ OPTS_LIBC=""
 
 # Install packages
 if hash apt-get 2>/dev/null; then
+
 	# This works for Debian 8 and Ubuntu 16.04
 	apt-get install wget make mingw-w64 gcc g++ bzip2 git autoconf texinfo
+
 elif hash yum 2>/dev/null; then
+
 	# This works for CentOS 7
 	yum install wget git texinfo
+
 	rpm -q epel-release-7-6.noarch >/dev/null
 	if [ $? -ne 0 ]; then
 		# EPEL is for the MinGW stuff
@@ -85,16 +101,27 @@ elif hash yum 2>/dev/null; then
 		wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel//7/x86_64/e/epel-release-7-6.noarch.rpm
 		rpm -Uvh epel-release-7-6.noarch.rpm
 	fi
+
 	yum install make mingw64-gcc mingw64-gcc-c++ mingw32-gcc mingw32-gcc-c++ gcc gcc-c++ bzip2 autoconf
+
 elif hash pacman 2>/dev/null; then
+
 	# Things have changed with Arch and this is now broken :/
 	pacman -S --needed wget make mingw-w64-binutils mingw-w64-gcc mingw-w64-crt mingw-w64-headers mingw-w64-winpthreads gcc bzip2 git autoconf texinfo
+
 fi
 
 # Stop on errors
 set -e
 
 TIME_START=$(date +%s)
+
+LOG_DIR=$(pwd)
+log()
+{
+	echo "$1"
+	echo "[$(date +"%d %b %y %H:%M:%S")]: $1" >> "$LOG_DIR/avr-gcc-build.log"
+}
 
 makeDir()
 {
@@ -125,13 +152,15 @@ fixGCCAVR()
 	echo "$ORIGINAL" >> ../gcc/config/avr/avr.c
 }
 
-echo "Clearing output directories..."
+log "Start"
+
+log "Clearing output directories..."
 [ $FOR_LINUX -eq 1 ] && makeDir "$PREFIX_GCC_LINUX"
 [ $FOR_WINX86 -eq 1 ] && makeDir "$PREFIX_GCC_WINX86"
 [ $FOR_WINX64 -eq 1 ] && makeDir "$PREFIX_GCC_WINX64"
 [ $BUILD_LIBC -eq 1 ] && makeDir "$PREFIX_LIBC"
 
-echo "Clearing old downloads..."
+log "Clearing old downloads..."
 rm -f $NAME_BINUTILS.tar.xz
 rm -rf $NAME_BINUTILS/
 rm -f $NAME_GCC.tar.xz
@@ -141,7 +170,7 @@ rm -rf $NAME_GDB/
 rm -f $NAME_LIBC.tar.bz2
 rm -rf $NAME_LIBC/
 
-echo "Downloading sources..."
+log "Downloading sources..."
 [ $BUILD_BINUTILS -eq 1 ] && wget https://ftpmirror.gnu.org/binutils/$NAME_BINUTILS.tar.xz
 [ $BUILD_GCC -eq 1 ] && wget https://ftpmirror.gnu.org/gcc/$NAME_GCC/$NAME_GCC.tar.xz
 [ $BUILD_GDB -eq 1 ] && wget https://ftpmirror.gnu.org/gdb/$NAME_GDB.tar.xz
@@ -179,78 +208,99 @@ confMakeGDB()
 
 # Make AVR-Binutils
 if [ $BUILD_BINUTILS -eq 1 ]; then
-	echo "Making Binutils..."
-	echo "Extracting..."
+	log "***Binutils***"
+	log "Extracting..."
 	tar xf $NAME_BINUTILS.tar.xz
 	mkdir -p $NAME_BINUTILS/obj-avr
 	cd $NAME_BINUTILS/obj-avr
+
+	log "Making for Linux..."
 	[ $FOR_LINUX -eq 1 ] && confMake "$PREFIX_GCC_LINUX" "$OPTS_BINUTILS"
+	log "Making for Windows x86..."
 	[ $FOR_WINX86 -eq 1 ] && confMake "$PREFIX_GCC_WINX86" "$OPTS_BINUTILS" --host=$HOST_WINX86 --build=`../config.guess`
+	log "Making for Windows x64..."
 	[ $FOR_WINX64 -eq 1 ] && confMake "$PREFIX_GCC_WINX64" "$OPTS_BINUTILS" --host=$HOST_WINX64 --build=`../config.guess`
+
 	cd ../../
 else
-	echo "Skipping Binutils..."
+	log "Skipping Binutils..."
 fi
 
 # Make AVR-GCC
 if [ $BUILD_GCC -eq 1 ]; then
-	echo "Making GCC..."
-	echo "Extracting..."
+	log "***GCC***"
+	log "Extracting..."
 	tar xf $NAME_GCC.tar.xz
 	mkdir -p $NAME_GCC/obj-avr
 	cd $NAME_GCC
+	
+	log "Getting prerequisites..."
 	chmod +x ./contrib/download_prerequisites
 	./contrib/download_prerequisites
+
 	cd obj-avr
 	# fixGCCAVR
+
+	log "Making for Linux..."
 	[ $FOR_LINUX -eq 1 ] && confMake "$PREFIX_GCC_LINUX" "$OPTS_GCC"
+	log "Making for Windows x86..."
 	[ $FOR_WINX86 -eq 1 ] && confMake "$PREFIX_GCC_WINX86" "$OPTS_GCC" --host=$HOST_WINX86 --build=`../config.guess`
+	log "Making for Windows x64..."
 	[ $FOR_WINX64 -eq 1 ] && confMake "$PREFIX_GCC_WINX64" "$OPTS_GCC" --host=$HOST_WINX64 --build=`../config.guess`
+
 	cd ../../
 else
-	echo "Skipping GCC..."
+	log "Skipping GCC..."
 fi
 
 # Make GDB
 if [ $BUILD_GDB -eq 1 ]; then
-	echo "Making GDB..."
-	echo "Extracting..."
+	log "***GDB***"
+	log "Extracting..."
 	tar xf $NAME_GDB.tar.xz
 	mkdir -p $NAME_GDB/obj-avr
 	cd $NAME_GDB/obj-avr
+	
+	log "Making for Linux..."
 	[ $FOR_LINUX -eq 1 ] && confMakeGDB "strip gdb/gdb" "$PREFIX_GCC_LINUX" "$OPTS_GDB"
+	log "Making for Windows x86..."
 	[ $FOR_WINX86 -eq 1 ] && confMakeGDB "$HOST_WINX86-strip gdb/gdb.exe" "$PREFIX_GCC_WINX86" "$OPTS_GDB" --host=$HOST_WINX86 --build=`../config.guess`
+	log "Making for Windows x64..."
 	[ $FOR_WINX64 -eq 1 ] && confMakeGDB "$HOST_WINX64-strip gdb/gdb.exe" "$PREFIX_GCC_WINX64" "$OPTS_GDB" --host=$HOST_WINX64 --build=`../config.guess`
+
 	cd ../../
 else
-	echo "Skipping GDB..."
+	log "Skipping GDB..."
 fi
 
 # Make AVR-LibC
 if [ $BUILD_LIBC -eq 1 ]; then
-	echo "Making AVR-LibC..."
+	log "***AVR-LibC***"
 	if [ "$NAME_LIBC" = "avr-libc3.git" ]; then
-		echo "Preparing..."
+		log "Preparing..."
 		cd $NAME_LIBC
 		git checkout $COMMIT_LIBC
 		./bootstrap
 		cd ..
 	else
-		echo "Extracting..."
+		log "Extracting..."
 		bunzip2 -c $NAME_LIBC.tar.bz2 | tar xf -
 	fi
 	mkdir -p $NAME_LIBC/obj-avr
 	cd $NAME_LIBC/obj-avr
+	
+	log "Making..."
 	confMake "$PREFIX_LIBC" "$OPTS_LIBC" --host=avr --build=`../config.guess`
+
 	cd ../../
 else
-	echo "Skipping AVR-LibC..."
+	log "Skipping AVR-LibC..."
 fi
 
 TIME_END=$(date +%s)
 TIME_RUN=$(($TIME_END - $TIME_START))
 
-echo ""
-echo "Done in $TIME_RUN seconds"
+log ""
+log "Done in $TIME_RUN seconds"
 
 exit 0
