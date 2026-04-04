371c36219ddd8eae228c30bbaf6982e7aa08c4f4
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2026-04-03 21:54:04 -0400

    Fix prerequisites when not building WIN versions

--- avr-gcc-build.sh
+++ avr-gcc-build.sh
@@ -147,7 +147,10 @@ log()
 
 installPackages()
 {
-	local requiredPackages=("wget" "make" "mingw-w64" "gcc" "g++" "bzip2" "xz-utils" "autoconf" "texinfo" "libgmp-dev" "libmpfr-dev" "libexpat1-dev")
+	local requiredPackages=("wget" "make" "gcc" "g++" "bzip2" "xz-utils" "autoconf" "texinfo" "libgmp-dev" "libmpfr-dev" "libexpat1-dev")
+	if [[ $FOR_WINX64 -ne 0 || $FOR_WINX86 -ne 0 ]]; then
+		requiredPackages+=("mingw-w64")
+	fi
 
 	if [[ $EUID -ne 0 ]]; then
 		log "Not running as root user. Checking whether all required packages are installed..."
